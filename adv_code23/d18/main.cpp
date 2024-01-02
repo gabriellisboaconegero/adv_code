1ecc0735b865a4840ba0766d4b1b0f52c33c8c56
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2024-01-02 16:59:08 -0300

    dia 18/23 feito !!!!!!!!!!!!!!!!!!

--- adv_code23/d18/main.cpp
+++ adv_code23/d18/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 struct point_t {
     i64 x;
     i64 y;
-    i64 hex;
 };
 
 void part1(char *input){
@@ -16,15 +15,16 @@ void part1(char *input){
         return;
 
     string line;
-    vector<struct point_t> pts(1, {0, 0, 0});
+    vector<struct point_t> pts(1, {0, 0});
     char dir;
     int num;
     i64 x, y, b;
     x = y = 0;
     b = 1;
+    i64 h;
     while (getline(file, line)){
         struct point_t p;
-        sscanf(line.data(), "%c %d (#%x)", &dir, &num, &p.hex);
+        sscanf(line.data(), "%c %d (#%x)", &dir, &num, &h);
         switch (dir){
             case 'U':
                 y += num;
@@ -46,7 +46,7 @@ void part1(char *input){
     }
     file.close();
 
-    pts.push_back({0, 0, 0});
+    pts.push_back({0, 0});
 
     i64 a = 0;
     // shoelace theorem
@@ -59,8 +59,67 @@ void part1(char *input){
     cout << "Part1: " << res << endl;
 }
 
+char dir_map[4] = {
+    'R',
+    'D',
+    'L',
+    'U'
+};
+
+void part2(char *input){
+    ifstream file;
+    file.open(input);
+    if  (!file.is_open())
+        return;
+
+    string line;
+    vector<struct point_t> pts(1, {0, 0});
+    char dir;
+    i64 x, y, b, num;
+    x = y = 0;
+    b = 1;
+    i64 h;
+    while (getline(file, line)){
+        struct point_t p;
+        sscanf(line.data(), "%c %d (#%x)", &dir, &num, &h);
+        num = h / 16;
+        switch (dir_map[h % 16]){
+            case 'U':
+                y += num;
+                break;
+            case 'D':
+                y -= num;
+                break;
+            case 'R':
+                x += num;
+                break;
+            case 'L':
+                x -= num;
+                break;
+        }
+        p.x = x;
+        p.y = y;
+        b += num;
+        pts.push_back(p);
+    }
+    file.close();
+
+    pts.push_back({0, 0});
+
+    i64 a = 0;
+    // shoelace theorem
+    for (size_t k = 0; k < pts.size()-1; k++)
+        a += (pts[k].y + pts[k+1].y)*(pts[k+1].x - pts[k].x);
+    a /= 2;
+
+    // pick's theorem
+    i64 res = a + 1 + b/2;
+    cout << "Part2: " << res << endl;
+}
+
 int main(int argc, char **argv){
     part1(argv[1]);
+    part2(argv[1]);
 
     return 0;
 }
