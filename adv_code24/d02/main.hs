f19d871c5531421287a3ec0e7b5263fa4e6c946b
gabriellisboaconegero <gabriellisboaconegero@gmail.com>
2024-12-19 19:51:21 -0300

    dia 02/24 feito !!

---
+++ adv_code24/d02/main.hs
@@ -0,0 +1,34 @@
+import System.IO
+import Data.Ix
+
+fileName :: Int -> String
+fileName 0 = "input.txt"
+fileName _ = "teste.txt"
+
+parseInput :: String -> [[Int]]
+parseInput = map (map read . words) . lines
+
+(>?) :: Ord a => (a, a) -> Bool
+(>?) (x, y) = x > y
+
+(<?) :: Ord a => (a, a) -> Bool
+(<?) (x, y) = (>?) (y, x)
+
+isSafe :: [Int] -> Bool
+isSafe xs = (all (safeFunc (<?)) ys) || (all (safeFunc (>?)) ys)
+    where
+        ys = zip xs (tail xs)
+        safeFunc :: ((Int, Int) -> Bool) -> (Int, Int) -> Bool
+        safeFunc f (x, y) = f (x, y) && inRange (1, 3) (abs (x - y))
+
+matrizGen :: [Int] -> [[Int]]
+matrizGen xs = xs : [take i xs ++ drop (i + 1) xs | i <- [0 .. length xs - 1]]
+
+solve1 :: String -> IO ()
+solve1 = print . length . filter isSafe . parseInput
+
+solve2 :: String -> IO ()
+solve2 = print . length . filter (any isSafe) . map matrizGen . parseInput
+
+main :: IO ()
+main = readFile (fileName 0) >>= solve2
